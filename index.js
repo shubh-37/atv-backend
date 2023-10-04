df65b87efc7a67eaa10e651961f12b5efb783912
shubh-37 <shubh.arya37@gmail.com>
2023-10-04 23:02:53 +0530

    config for multer

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
-
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: './uploads', // Directory where uploaded files will be stored
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // Use the original file name for the stored file
+    },
+  });
+  export const upload = multer({ storage });
 app.get('/', ((req,res) => {
     res.send('Hello world');
 }));
