270f4d7d2296c0b2531156144d940c05d7344d50
shubh-37 <shubh.arya37@gmail.com>
2023-10-05 23:18:20 +0530

    fixed issues

--- models/Product.js
+++ models/Product.js
@@ -1,35 +1,35 @@
 function createProduct(sequelize, DataTypes) {
-    const Product = sequelize.define(
-      "Product",
-      {
-        barcode: {
-          type: DataTypes.STRING,
-          primaryKey: true,
-        },
-        imageUrl: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          field: image_url
-        },
-        categoryOne: {
-          type: DataTypes.STRING,
-          field: category_one
-        },
-        categoryTwo: {
-          type: DataTypes.STRING,
-          field: category_two
-        },
-        categoryThree: {
-          type: DataTypes.STRING,
-          field: category_three
-        }
+  const Product = sequelize.define(
+    "Product",
+    {
+      barcode: {
+        type: DataTypes.STRING,
+        primaryKey: true,
       },
-      {
-        timestamps: false,
-        tableName: "products",
-      }
-    );
-    return Product;
-  }
-  
-  module.exports = createProduct;
+      imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "image_url",
+      },
+      categoryOne: {
+        type: DataTypes.STRING,
+        field: "category_one",
+      },
+      categoryTwo: {
+        type: DataTypes.STRING,
+        field: "category_two",
+      },
+      categoryThree: {
+        type: DataTypes.STRING,
+        field: "category_three",
+      },
+    },
+    {
+      timestamps: false,
+      tableName: "products",
+    }
+  );
+  return Product;
+}
+
+module.exports = createProduct;
