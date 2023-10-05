af6b8bc46ae98ae6f6be3ca2920779094ecb04b1
shubh-37 <shubh.arya37@gmail.com>
2023-10-05 23:17:37 +0530

    db connection and removed multer

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: './uploads', // Directory where uploaded files will be stored
-    filename: function (req, file, cb) {
-      cb(null, file.originalname); // Use the original file name for the stored file
-    },
-  });
-  export const upload = multer({ storage });
-app.get('/', ((req,res) => {
-    res.send('Hello world');
-}));
+const cors = require("cors");
 
-app.listen(3001, () => {
-    console.log("Server is listening on port:3001");
+var sequelize = null;
+const dbConnection = require("./dbConnection/connect");
+const productController = require('./controllers/products');
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  res.append("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(cors());
+
+
+async function start() {
+  try {
+    if (!sequelize) {
+      sequelize = await dbConnection();
+    } else {
+      // restart connection pool to ensure connections are not re-used across invocations
+      sequelize.connectionManager.initPools();
+      // restore `getConnection()` if it has been overwritten by `close()`
+      if (sequelize.connectionManager.hasOwnProperty("getConnection")) {
+        delete sequelize.connectionManager.getConnection;
+      }
+    }
+    productController(app, sequelize);
+    app.listen(3001, () => {
+      console.log("Server is listening on port:3001");
+    });
+  } catch (error) {
+    console.log({error});
+  }
+}
+
+start();
