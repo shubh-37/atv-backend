2e7939d886e2b0809e3e65b6132ddeb8535ff9c1
shubh-37 <shubh.arya37@gmail.com>
2023-10-07 11:06:35 +0530

    first iteration of all CRUD apis

--- controllers/products.js
+++ controllers/products.js
@@ -16,26 +16,24 @@ function productController(app, Models) {
         return res.status(400).send("No file uploaded.");
       }
       console.log("File uploaded successfully");
-      console.log(req.file);
       const { barcode, ...categories } = req.body;
-
-      console.log({ barcode });
-
-      // const productInstance = await Product.create({ barcode, imageUrl }); //how to add categories
-      // if (productInstance) {
-      //   return res.status(201).json({
-      //     message: "Product entry added successfully",
-      //     productInstance,
-      //   });
-      // }
-      // return res
-      //   .status(400)
-      //   .json({ message: "Error creating product, please try again." });
+      const imageUrl = req.file.path;
+      const productInstance = await Product.create({ barcode, imageUrl }); //how to add categories
+      console.log({ productInstance });
+      if (productInstance) {
+        return res.status(201).json({
+          message: "Product entry added successfully",
+          productInstance,
+        });
+      }
+      return res
+        .status(400)
+        .json({ message: "Error creating product, please try again." });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
   });
-  app.get("/", async function getAllProducts(req, res) {
+  app.get("/products", async function getAllProducts(req, res) {
     try {
       const allProducts = await Product.findAll({});
       if (allProducts.length > 0) {
@@ -46,13 +44,51 @@ function productController(app, Models) {
       return res.status(500).json({ message: error.message });
     }
   });
-  app.put("/", async function editProduct(req, res) {
+  app.put("/:barcode", async function editProduct(req, res) {
     try {
-    } catch (error) {}
+      const { barcode } = req.params;
+      const { imageUrl, ...categories } = req.body;
+      if (barcode.length > 0) {
+        const updatedProduct = await Product.update(
+          { barcode },
+          {
+            where: {
+              imageUrl,
+            },
+          }
+        );
+        console.log(updatedProduct);
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   });
-  app.delete("/", async function deleteProduct(req, res) {
+  app.delete("/:barcode", async function deleteProduct(req, res) {
     try {
-    } catch (error) {}
+      const { barcode } = req.params;
+      if (barcode.length > 0) {
+        const deletedProduct = await Product.destroy({
+          where: {
+            barcode,
+          },
+        });
+        if (deletedProduct) {
+          return res.status(200).json({
+            message: `Product with barcode: ${barcode} deleted successfully`,
+          });
+        } else {
+          return res
+            .status(200)
+            .json({ message: `No product exists with barcode: ${barcode}` });
+        }
+      } else {
+        return res
+          .status(400)
+          .json({ message: "No barcode receieved, please send barcode." });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   });
 }
 
