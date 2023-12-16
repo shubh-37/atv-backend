6db480be07dee6dff621148b56f4a220fd3af458
Shubh Arya <shubh.arya37@gmail.com>
2023-12-16 18:00:13 +0530

    removed signup api and fixed login api

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 function userDefinedException(message, statusCode) {
@@ -7,39 +6,6 @@ function userDefinedException(message, statusCode) {
 }
 function auth(app, sequelize) {
   const { User } = sequelize;
-  app.post("/register", async function register(req, res) {
-    const { username, password } = req.body;
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      const user = await User.create({
-        username: username,
-        password: hashedPassword,
-      });
-      if (user) {
-        console.log(user.username);
-        const token = jwt.sign(
-          { username: user.username },
-          process.env.JWT_SECRET,
-          {
-            expiresIn: "24h",
-          }
-        );
-        return res.status(201).json({ token: token, username: username });
-      } else {
-        throw new userDefinedException(
-          "Error creating user, please try after some time",
-          400
-        );
-      }
-    } catch (error) {
-      if (error.message.match("E11000 duplicate key error collection:")) {
-        return res.status(409).json({ message: "Duplicate User" });
-      }
-      return res.status(500).json({ message: error.message });
-    }
-  });
-
   app.post("/login", async function login(req, res) {
     const { username, password } = req.body;
     try {
@@ -54,11 +20,9 @@ function auth(app, sequelize) {
       if (!user) {
         throw new userDefinedException("No user found!", 404);
       }
-      const isPasswordCorrect = await bcrypt.compare(password, user.password);
-      if (!isPasswordCorrect) {
+      if (password !== user.password) {
         throw new userDefinedException("Wrong password!", 401);
       }
-
       const token = jwt.sign(
         { username: user.username },
         process.env.JWT_SECRET,
