6f971115cc850d5b8412657a6c90127d437644ff
Shubh Arya <shubh.arya37@gmail.com>
2024-02-13 23:07:55 +0530

    added model changes for the new login design

--- models/User.js
+++ models/User.js
@@ -10,6 +10,37 @@ function createUser(sequelize, DataTypes) {
           type: DataTypes.STRING,
           allowNull: false,
         },
+        mobileNumber: {
+          type: DataTypes.NUMBER,
+          allowNull: false,
+          field: 'mobile_number'
+        },
+        retryCount: {
+          type: DataTypes.NUMBER,
+          defaultValue: 0,
+          validate: {
+            max: {
+              args: 3,
+              msg: 'Max retry count reached!'
+            }
+          },
+          field: 'retry_count'
+        },
+        otp: {
+          type: DataTypes.NUMBER,
+          validate: {
+            max: {
+              args: 4,
+              msg: 'Otp should be less than 4 characters'
+            }
+        }
+      },
+        otpGeneratedAt: {
+          type: DataTypes.DATE,
+          allowNull: true,
+          defaultValue: null,
+          field: 'otp_generated_at'
+        }
       },
       {
         timestamps: false,
