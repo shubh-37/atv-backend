1a62dafef34d1a6fc73f88ab1100f6fe6fd41a0f
Shubh Arya <shubh.arya37@gmail.com>
2024-02-18 18:58:16 +0530

    resolved PR comments

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-function userDefinedException(message, statusCode) {
-  this.message = message;
-  this.statusCode = statusCode;
-}
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+const MAX_RETRY_COUNT = 4;
 
 function isStoredTimeWithin10Minutes(otpGeneratedAt) {
   // Convert stored time to Date object
@@ -31,70 +29,55 @@ function auth(app, sequelize) {
     const { mobileNumber } = req.body;
     try {
       if (!mobileNumber) {
-        throw new userDefinedException('Please enter mobile number', 401);
+        return res.status(401).json({ message: 'Please provide with mobile number!' });
       }
       const user = await User.findOne({ where: { mobileNumber } });
       if (!user) {
-        throw new userDefinedException('No user found!', 404);
+        return res.status(404).json({ message: 'User not found!' });
       }
 
       if (!user.otp) {
-        const randomInt = getRandomInt(1000, 9999); // Generates a random integer between 1 and 100
-        console.log(randomInt);
+        const randomInt = getRandomInt(1000, 9999); // Generates a random integer between 1000 and 9999
         const otpGeneratedAt = new Date();
         let retryCount = user.retryCount + 1;
         //add send Interakt notification function
         const updatedUser = await user.update({
-          otp,
+          otp: randomInt,
           retryCount,
           otpGeneratedAt
         });
         console.log(updatedUser);
-        return res
-          .status(200)
-          .json({ message: `Otp sent successfully to ${mobileNumber}` });
+        return res.status(200).json({ message: `Otp sent successfully to ${mobileNumber}` });
+      }
+      const isOtpValid = isStoredTimeWithin10Minutes(user.otpGeneratedAt);
+      if (user.retryCount < MAX_RETRY_COUNT && isOtpValid) {
+        let retryCount = user.retryCount + 1;
+        const updatedUser = await user.update({
+          retryCount
+        });
+        console.log(updatedUser);
+        //add interakt notification
+        return res.status(200).json({ message: `Otp sent successfully to ${mobileNumber}` });
+      } else if (!isOtpValid) {
+        console.log('Session timed out, try logging again');
+        //clear otp, retry_count and otpGeneratedAt
+        const updatedUser = await user.update({
+          otp: null,
+          retryCount: 0,
+          otpGeneratedAt: null
+        });
+        console.log(updatedUser);
+        return res.status(440).json({ message: 'Session timed out. Please login again' });
       } else {
-        let isOtpValid;
-        isOtpValid = isStoredTimeWithin10Minutes(user.otpGeneratedAt);
-        if (user.retryCount < 3) {
-          if (isOtpValid) {
-            let retryCount = user.retryCount + 1;
-            const updatedUser = await user.update({
-              otp,
-              retryCount,
-              otpGeneratedAt
-            });
-            console.log(updatedUser);
-            //add interakt notification
-            return res
-              .status(200)
-              .json({ message: `Otp sent successfully to ${mobileNumber}` });
-          } else {
-            console.log('Session timed out, try logging again');
-            //clear otp, retry_count and otpGeneratedAt
-            const updatedUser = await user.update({
-              otp: null,
-              retryCount: 0,
-              otpGeneratedAt: null
-            });
-            console.log(updatedUser);
-            return res
-              .status(440)
-              .json({ message: 'Session timed out. Please login again' });
-          }
-        } else {
-          console.log('max retry count reached');
-          //clear otp, retry_count and otpGeneratedAt
-          const updatedUser = await user.update({
-            otp: null,
-            retryCount: 0,
-            otpGeneratedAt: null
-          });
-          console.log(updatedUser);
-          return res
-            .status(429)
-            .json({ message: 'Max retry count reached. Please login again' });
-        }
+        console.log('max retry count reached');
+        //clear otp, retry_count and otpGeneratedAt
+        const updatedUser = await user.update({
+          otp: null,
+          retryCount: 0,
+          otpGeneratedAt: null
+        });
+        console.log(updatedUser);
+        return res.status(429).json({ message: 'Max retry count reached. Please login again' });
       }
     } catch (error) {
       console.log(error);
@@ -104,24 +87,19 @@ function auth(app, sequelize) {
   app.get('/verifyOtp', async function verifyOtp(req, res) {
     const { mobileNumber, otp } = req.body;
     try {
-      if (!otp) {
-        throw new userDefinedException('OTP is not present', 401);
+      if (!otp && !mobileNumber) {
+        return res.status(401).json({ message: 'Please provide with mobile number and OTP!' });
       }
       const user = await User.findOne({ where: { mobileNumber } });
       if (!user) {
-        throw new userDefinedException('No user found!', 404);
+        return res.status(404).json({ message: 'User not found!' });
       }
-      let isOtpValid;
-      isOtpValid = isStoredTimeWithin10Minutes(user.otpGeneratedAt);
+      const isOtpValid = isStoredTimeWithin10Minutes(user.otpGeneratedAt);
       if (isOtpValid) {
         if (user.otp === otp) {
-          const token = jwt.sign(
-            { username: user.username },
-            process.env.JWT_SECRET,
-            {
-              expiresIn: '24h'
-            }
-          );
+          const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
+            expiresIn: '24h'
+          });
           //reset everything
           const updatedUser = await user.update({
             otp: null,
@@ -131,22 +109,17 @@ function auth(app, sequelize) {
           console.log(updatedUser);
           return res.status(200).json({ message: `Login successful`, token });
         } else {
-          return res
-            .status(401)
-            .json({ message: 'Incorrect OTP. Please try again' });
+          return res.status(401).json({ message: 'Incorrect OTP. Please try again' });
         }
-      } else {
-        console.log('go back to the login page and send otp again');
-        const updatedUser = await user.update({
-          otp: null,
-          retryCount: 0,
-          otpGeneratedAt: null
-        });
-        console.log(updatedUser);
-        return res
-          .status(440)
-          .json({ message: 'Session timed out. Please login again' });
       }
+      console.log('go back to the login page and send otp again');
+      const updatedUser = await user.update({
+        otp: null,
+        retryCount: 0,
+        otpGeneratedAt: null
+      });
+      console.log(updatedUser);
+      return res.status(440).json({ message: 'Session timed out. Please login again' });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: error.message });
