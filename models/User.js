0ee78f45f7c558fcb80de3f3e89f54ee41de809b
Shubh Arya <shubh.arya37@gmail.com>
2024-02-25 10:31:45 +0530

    made changes in user model

--- models/User.js
+++ models/User.js
@@ -1,53 +1,54 @@
 function createUser(sequelize, DataTypes) {
-    const User = sequelize.define(
-      "User",
-      {
-        username: {
-          type: DataTypes.STRING,
-          primaryKey: true,
+  const User = sequelize.define(
+    'User',
+    {
+      username: {
+        type: DataTypes.STRING,
+        primaryKey: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      mobileNumber: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        field: 'mobile_number'
+      },
+      retryCount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: {
+          max: {
+            args: 3,
+            msg: 'Max retry count reached!'
+          }
         },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        mobileNumber: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          field: 'mobile_number'
-        },
-        retryCount: {
-          type: DataTypes.INTEGER,
-          defaultValue: 0,
-          validate: {
-            max: {
-              args: 3,
-              msg: 'Max retry count reached!'
+        field: 'retry_count'
+      },
+      otp: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isFourDigits(value) {
+            if (value && value.toString().length !== 4) {
+              throw new Error('OTP should be exactly 4 digits');
             }
-          },
-          field: 'retry_count'
-        },
-        otp: {
-          type: DataTypes.INTEGER,
-          validate: {
-            max: {
-              args: 4,
-              msg: 'Otp should be less than 4 characters'
-            }
+          }
         }
       },
-        otpGeneratedAt: {
-          type: DataTypes.DATE,
-          allowNull: true,
-          defaultValue: null,
-          field: 'otp_generated_at'
-        }
-      },
-      {
-        timestamps: false,
-        tableName: "users",
+      otpGeneratedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+        field: 'otp_generated_at'
       }
-    );
-    return User;
-  }
-  
-  module.exports = createUser;
+    },
+    {
+      timestamps: false,
+      tableName: 'users'
+    }
+  );
+  return User;
+}
+
+module.exports = createUser;
