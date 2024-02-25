46058fa81ff858a88f5c125a6bf62e3f93c3ecae
Shubh Arya <shubh.arya37@gmail.com>
2024-02-25 10:32:21 +0530

    minor changes related to response and error handling

--- middleware/sendInteraktNotification.js
+++ middleware/sendInteraktNotification.js
@@ -1,23 +1,42 @@
-export async function sendInteraktNotification(user, whatsappSetting) {
-  const { mobileNumber, otp } = user
-  const { eventName, interaktAuthToken } = whatsappSetting
-  const countryCode = '+91'
-  const traits = {}
-  traits['otp'] = otp
-  const response = await axiosInstance.post(
-    'https://api.interakt.ai/v1/public/track/events/',
-    {
-      phoneNumber: mobileNumber,
-      countryCode,
-      eventName,
-      traits
-    },
-    {
-      headers: {
-        Authorization: interaktAuthToken
+const axios = require('axios');
+
+const axiosInstance = axios.create();
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error || error.data);
+  }
+);
+
+async function sendInteraktNotification(userDetails, eventName) {
+  const { phoneNumber, otp } = userDetails;
+  const countryCode = '91';
+  const traits = {};
+  traits['otp'] = otp;
+  try {
+    const response = await axiosInstance.post(
+      'https://api.interakt.ai/v1/public/track/events/',
+      {
+        phoneNumber,
+        countryCode,
+        event: eventName,
+        traits
+      },
+      {
+        headers: {
+          Authorization: process.env.INTERAKT_AUTH_TOKEN
+        }
       }
+    );
+    // console.log({ response });
+    return Promise.resolve({ message: 'Event Recorded Successfully', result: response.result });
+  } catch (error) {
+    if (!error.response.data.result) {
+      return Promise.reject({ result: false, message: 'Unable to find the user.' });
     }
-  )
-  console.log(response)
-  return Promise.resolve({ message: 'Event Recorded Successfully' })
+  }
 }
+
+module.exports = sendInteraktNotification;
